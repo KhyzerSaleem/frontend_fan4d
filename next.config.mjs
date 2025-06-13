1bbf9bffd9546af14c23494ece94da43bf09d801
Khizar Saleem <62766582+KhyzerSaleem@users.noreply.github.com>
2025-06-14 02:34:48 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
   typescript: {
     ignoreBuildErrors: true, 
